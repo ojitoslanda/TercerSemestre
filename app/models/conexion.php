3e9325bd3aa9cf1cdd0af2d29f1e6cda5ccc99d1
ojitoslanda <ojitoslanda@gmail.com>
2025-04-26 15:23:21 -0500

    update

--- app/models/conexion.php
+++ app/models/conexion.php
@@ -1,12 +1,39 @@
 <?php
-//Variables
-$servidor = "localhost";
-$user = "root";
-$clave = "";
-$database = "minimarket_db";
-$conexion = new mysqli($servidor,$user,$clave,$database);
-if($conexion->connect_error){
-    die("ERROR EN LA CONEXION: ".$conexion->connect_error);
-}else{
-    echo "OK";
+class Conexion{
+    //Atributos
+    private $servidor;
+    private $user;
+    private $clave;
+    private $database;
+    private $conexion;
+    //Constructor
+    public function __construct(){
+        $this->servidor = "localhost";
+        $this->user = "root";
+        $this->clave = "";
+        $this->database = "minimarket_db";
+    }
+    public function __construct(){ }
+
+
+    // Funcion para conectar
+    public function conectar(){
+        $this->conexion = new mysqli($this->servidor,$this->user,$this->clave,$this->database);
+    }
+    //Funcion para salir o cerrar conexion
+    public function cerrarConexion(){
+        $this->conexion->close();
+    }
+
+    //Metodo o funcion que devuelve un registro: SELECT
+    // GET -> para obtener
+    public function getEjecucionQuery($sql){
+
+    }
+    //Metodo o funcion que devuelve un valor : INSERT, UPDATE, DELETE
+    // SET para establecer o enviar DATOS
+    public function setEjecucionQuery($sql){
+        
+    }
 }
+
