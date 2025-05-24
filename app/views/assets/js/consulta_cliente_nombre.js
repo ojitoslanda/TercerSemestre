d59dc45083b4990ff40c71174783b8c3300f0d68
ojitoslanda <ojitoslanda@gmail.com>
2025-05-24 12:26:56 -0500

    asdasd

--- app/views/assets/js/consulta_cliente_nombre.js
+++ app/views/assets/js/consulta_cliente_nombre.js
@@ -1,6 +1,9 @@
 const variable_dni = document.getElementById("dni_cliente");
 variable_dni.addEventListener("keyup", function (){
     //Validar que el input acepte solo números
+    this.value = this.value.replace(/[^0-9]/g, '');
+    if(this.value.length === 10)
+
     
 });
 
